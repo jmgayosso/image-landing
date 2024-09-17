2bf5030dc758d2bcdea52a510083f4d27f48abfc
Jose Maria Gayosso <josemaria@hashed.io>
2024-09-16 21:10:19 -0600

    🔧 chore(vite.config.js): update base URL to 'https://jmgayosso.github.io/image-landing' for deployment on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-  // base: 'https://danipoot.github.io/image-landing'
+  // base: '/'
+  base: 'https://jmgayosso.github.io/image-landing'
 })
